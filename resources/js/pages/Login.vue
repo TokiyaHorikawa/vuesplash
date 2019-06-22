d070291be7d23de8a39efd3cb99205364ca6d076
TokiyaHorikawa <ayikot4423@gmail.com>
2019-06-23 02:26:25 +0900

    fix: gitのキャッシュ削除

---
+++ resources/js/pages/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container--small">
+    <ul class="tab">
+      <li class="tab__item" @click="tab = 1">Login</li>
+      <li class="tab__item" @click="tab = 2">Register</li>
+    </ul>
+    {{ tab }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tab: 1
+    }
+  }
+}
+</script>
