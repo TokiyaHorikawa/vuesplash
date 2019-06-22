1e9b1d90b68069307fb5683156d3159b024d3707
TokiyaHorikawa <ayikot4423@gmail.com>
2019-06-23 02:38:09 +0900

    feta: ログイン画面のボタン作成

--- resources/js/pages/Login.vue
+++ resources/js/pages/Login.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="container--small">
     <ul class="tab">
-      <li class="tab__item" @click="tab = 1">Login</li>
-      <li class="tab__item" @click="tab = 2">Register</li>
+      <li
+        class="tab__item"
+        :class="{ 'tab__item--active': tab === 1 }"
+        @click="tab = 1"
+      >Login</li>
+      <li
+        class="tab__item"
+        :class="{ 'tab__item--active': tab === 2 }"
+        @click="tab = 2"
+      >Register</li>
     </ul>
     {{ tab }}
   </div>
