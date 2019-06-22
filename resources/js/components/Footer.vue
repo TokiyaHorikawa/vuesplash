e3fbb89fc1ff6d2d9cc84f419e8ae5b9f50979f6
TokiyaHorikawa <ayikot4423@gmail.com>
2019-06-23 02:26:00 +0900

    fix: gitのキャッシュ削除

--- resources/js/components/Footer.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <footer class="footer">
-    <button class="button button--link">Logout</button>
-    <RouterLink class="button button--link" to="/login">
-      Login / Register
-    </RouterLink>
-  </footer>
-</template>
--- resources/js/pages/PhotoList.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <h1>Photo List</h1>
-</template>
