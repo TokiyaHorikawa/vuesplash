29743861c0e2583855d8ed459493f2e3eba49e78
TokiyaHorikawa <ayikot4423@gmail.com>
2019-06-29 19:52:09 +0900

    fix: タイポ修正 windows → window

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,11 +1,11 @@
 import { getCookieValue } from './util'
 
-windows.axios = require('axios')
+window.axios = require('axios')
 
 // Ajaxリクエストであることを示すヘッダーを付与する
-windows.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
 
-windows.axios.interceptors.request.use(config => {
+window.axios.interceptors.request.use(config => {
   // クッキーからトークンを取り出してヘッダーに添付する
   config.headers['X-XSRF-TOKEN'] = getCookieValue('XSRF-TOKEN')
 
